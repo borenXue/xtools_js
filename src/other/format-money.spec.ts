9dc04547bf82b69f49c57304405c6ebc8e5e1391
borenXue <boren.xue@outlook.com>
2020-03-25 11:26:20 +0800

    fix(xtools.js): 修复 formatMoney 处理小数小于2位时没有返回2位小数

--- src/other/format-money.spec.ts
+++ src/other/format-money.spec.ts
@@ -3,6 +3,30 @@ import formatMoney from './format-money';
 
 describe('formatMoney 测试用例', () => {
 
+  it('0 --> 0.00', () => {
+    expect(formatMoney(0)).equal('0.00')
+  })
+
+  it('-0 --> 0.00', () => {
+    expect(formatMoney(-0)).equal('0.00')
+  })
+
+  it('-1 --> -1.00', () => {
+    expect(formatMoney(-1)).equal('-1.00')
+  })
+
+  it('1 --> 1.00', () => {
+    expect(formatMoney(1)).equal('1.00')
+  })
+
+  it('1.2 --> 1.20', () => {
+    expect(formatMoney(1.2)).equal('1.20')
+  })
+
+  it('.2 --> 0.20', () => {
+    expect(formatMoney(.2)).equal('0.20')
+  })
+
   it('100.348 --> -100.35', () => {
     expect(formatMoney(-100.345)).equal('-100.35')
   })
@@ -21,10 +45,6 @@ describe('formatMoney 测试用例', () => {
 
   it('5123456789.348 --> 5,123,456,789.35', () => {
     expect(formatMoney(5123456789.348)).equal('5,123,456,789.35')
-  })
-
-  it('5123456789.348 --> 5,123,456,789.35', () => {
-    expect(formatMoney(0)).equal('0')
   })
 
 })
--- src/other/format-money.ts
+++ src/other/format-money.ts
@@ -3,6 +3,9 @@ export default function formatMoney(money: number) {
   const num = isNaN(+money) ? 0 : +money;
   const prefix = num < 0 ? '-' : '';
   let str = String(Math.round(Math.abs(num) * 100) / 100);
+  if (str.indexOf('.') === 0) str = '0' + str;
+  if (str.indexOf('.') < 0) str += '.00';
+  if (str.indexOf('.') === (str.length - 2)) str += '0'
 
   const suffix = str.substring(str.indexOf('.'));
   str = str.substring(0, str.indexOf('.')).replace(/^-/, '');
