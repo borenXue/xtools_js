1f718bab794e17a9bdf9951622b905edbff39f22
borenXue <boren.xue@outlook.com>
2020-06-08 15:22:10 +0800

    fix(xtools.js): 修复 number 函数 - accAdd && accSub

--- src/number/index.ts
+++ src/number/index.ts
@@ -1,56 +1,70 @@
+import { padRight } from '../utils/index'
+
+/**
+ * Chrome 浏览器中的异常例子:
+ * 
+ * 1、2.24 + 2.22 = 4.460000000000001
+ *    2.24 - 2.22 = 0.020000000000000018
+ * 2、2.3 / 0.1 = 22.999999999999996
+ *    2.3 * 0.1 = 0.22999999999999998
+ * 3、-652.94 + 0.01 = -652.9300000000001
+ *    -652.44 + 0.01 = -652.4300000000001
+ * 4、652.94 * 100 = 65294.00000000001  ---> accAdd 与 accSub 中不可直接使用 num1*m 的方式
+ *    652.44 / 100 = 65294.00000000001
+ */
 
 /**
  * 加法函数
  */
 export function accAdd(num1: number = 0, num2: number = 0, ...rest: number[]): number {
-  const r1 = (num1.toString().split('.')[1] || '').length;
-  const r2 = (num2.toString().split('.')[1] || '').length;
-  const m = Math.pow(10, Math.max(r1, r2));
+  const r1Positive = num1.toString().split('.')[0] || '0';
+  const r2Positive = num2.toString().split('.')[0] || '0';
+  const r1Decimal = num1.toString().split('.')[1] || '';
+  const r2Decimal = num2.toString().split('.')[1] || '';
+
+  const decimalMaxCount = Math.max(r1Decimal.length, r2Decimal.length);
+  const m = Math.pow(10, decimalMaxCount);
+
+  const r1Num = +(r1Positive + padRight(r1Decimal, decimalMaxCount, '0'))
+  const r2Num = +(r2Positive + padRight(r2Decimal, decimalMaxCount, '0'))
+
+  const result = (r1Num + r2Num) / m;
 
   if (rest && rest.length > 0) {
     return accAdd(
-      (num1 * m + num2 * m) / m,
+      result,
       rest.shift() as number,
       ...rest,
     );
   }
-  return (num1 * m + num2 * m) / m;
+  return result;
 }
 
 /**
  * 减法函数
  */
 export function accSub(num1: number = 0, num2: number = 0, ...rest: number[]): number {
-  // const r1 = (num1.toString().split('.')[1] || '').length;
-  // const r2 = (num2.toString().split('.')[1] || '').length;
-  // const rMax = Math.max(r1, r2);
-  // const m = Math.pow(10, rMax);
+  const r1Positive = num1.toString().split('.')[0] || '0';
+  const r2Positive = num2.toString().split('.')[0] || '0';
+  const r1Decimal = num1.toString().split('.')[1] || '';
+  const r2Decimal = num2.toString().split('.')[1] || '';
 
-  // num1 = +(String(num1).replace('.', '')) * Math.pow(10, rMax - r1);
-  // num2 = +(String(num2).replace('.', '')) * Math.pow(10, rMax - r2);
+  const decimalMaxCount = Math.max(r1Decimal.length, r2Decimal.length);
+  const m = Math.pow(10, decimalMaxCount);
 
-  // if (rest && rest.length > 0) {
-  //   return accSub(
-  //     (num1 - num2) / m,
-  //     rest.shift() as number,
-  //     ...rest,
-  //   );
-  // }
+  const r1Num = +(r1Positive + padRight(r1Decimal, decimalMaxCount, '0'))
+  const r2Num = +(r2Positive + padRight(r2Decimal, decimalMaxCount, '0'))
 
-  // return (num1 - num2) / m;
-
-  const r1 = (num1.toString().split('.')[1] || '').length;
-  const r2 = (num2.toString().split('.')[1] || '').length;
-  const m = Math.pow(10, Math.max(r1, r2));
+  const result = (r1Num - r2Num) / m;
 
   if (rest && rest.length > 0) {
     return accSub(
-      (num1 * m - num2 * m) / m,
+      result,
       rest.shift() as number,
       ...rest,
     );
   }
-  return (num1 * m - num2 * m) / m;
+  return result;
 }
 
 /**
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -11,3 +11,16 @@ export function padLeft(origin: string | number, digit: number, holder: string =
 
   return `${prefix}${str}`;
 }
+
+export function padRight(origin: string | number, digit: number, holder: string = ' ') {
+  const str = typeof origin === 'string' ? origin : origin.toString()
+
+  if (str.length >= digit) return str;
+
+  let suffix = '';
+  for (let i = 0; i < digit - str.length; i++) {
+    suffix += holder
+  }
+
+  return `${str}${suffix}`;
+}
