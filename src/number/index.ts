8ebe4245bcb5125c242a6090c6fc759b8f40700f
borenXue <boren.xue@outlook.com>
2020-06-08 16:43:29 +0800

    fix(xtools.js): 修复 number 函数 - accDiv

--- src/number/index.ts
+++ src/number/index.ts
@@ -91,18 +91,18 @@ export function accMulti(num1: number = 0, num2: number = 1, ...rest: number[]):
  * 除法函数
  */
 export function accDiv(num1: number, num2: number = 1, ...rest: number[]): number {
-  const r1 = (num1.toString().split('.')[1] || '').length;
-  const r2 = (num2.toString().split('.')[1] || '').length;
+  const r1Positive = num1.toString().split('.')[0] || '0';
+  const r2Positive = num2.toString().split('.')[0] || '0';
+  const r1Decimal = num1.toString().split('.')[1] || '';
+  const r2Decimal = num2.toString().split('.')[1] || '';
 
-  num1 = +(String(num1).replace('.', ''));
-  num2 = +(String(num2).replace('.', ''));
+  const decimalMaxCount = Math.max(r1Decimal.length, r2Decimal.length);
+  const m = Math.pow(10, decimalMaxCount);
 
-  let thisResult = undefined;
-  if (r2 > r1) {
-    thisResult = (num1 / num2) * Math.pow(10, r2-r1);
-  } else {
-    thisResult = (num1 / num2) / Math.pow(10, r1-r2);
-  }
+  const r1Num = +(r1Positive + padRight(r1Decimal, decimalMaxCount, '0'))
+  const r2Num = +(r2Positive + padRight(r2Decimal, decimalMaxCount, '0'))
+
+  let thisResult = r1Num / r2Num;
 
   if (rest && rest.length > 0) {
     return accDiv(
